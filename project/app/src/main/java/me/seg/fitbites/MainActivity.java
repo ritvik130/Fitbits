7a82cfe852a67717ec49b2a0f93bc71f80ee373b
KyleDulce <36104936+KyleDulce@users.noreply.github.com>
2021-09-25 01:11:17 -0400

    Debugged auth system

--- project/app/src/main/java/me/seg/fitbites/MainActivity.java
+++ project/app/src/main/java/me/seg/fitbites/MainActivity.java
@@ -3,6 +3,10 @@ package me.seg.fitbites;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import me.seg.fitbites.firebase.AuthManager;
+import me.seg.fitbites.firebase.OnTaskComplete;
 
 public class MainActivity extends AppCompatActivity {
 
--- project/app/src/main/java/me/seg/fitbites/firebase/AuthManager.java
+++ project/app/src/main/java/me/seg/fitbites/firebase/AuthManager.java
@@ -16,8 +16,12 @@ public class AuthManager {
     private FirebaseAuth authInstance;
 
     public AuthManager() {
+        if(instance != null && instance.authInstance.getCurrentUser() != null) {
+            authInstance = instance.authInstance;
+        } else {
+            authInstance = FirebaseAuth.getInstance();
+        }
         instance = this;
-        authInstance = FirebaseAuth.getInstance();
     }
 
     public void validateUser(String email, String password, OnTaskComplete<LoginResult> onComplete) {
@@ -25,6 +29,8 @@ public class AuthManager {
         if(email.equalsIgnoreCase("admin") && email.equals("admin123")) {
             onComplete.onComplete(new LoginResult(true));
         }
+        //created using example from Official Documentation:
+        //https://firebase.google.com/docs/auth/android/start#sign_up_new_users
         authInstance.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
@@ -41,6 +47,8 @@ public class AuthManager {
 
     public void createUser(String email, String password, OnTaskComplete<LoginResult> onComplete) {
         //create a new account
+        //created using example from Official Documentation:
+        //https://firebase.google.com/docs/auth/android/start#sign_in_existing_users
         authInstance.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
@@ -54,6 +62,10 @@ public class AuthManager {
                 onComplete.onComplete(new LoginResult(task.isSuccessful()));
             }
         });
+    }
+
+    public void signoutUser() {
+        authInstance.signOut();
     }
 
     //TODO: Setup User class with result
