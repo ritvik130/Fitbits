aa7d40d31ceb5bfbe6921f16c7fb96ba6b21cbef
KyleDulce <36104936+KyleDulce@users.noreply.github.com>
2021-10-03 15:02:42 -0400

    Added Screen

---
+++ project/app/src/main/java/me/seg/fitbites/InstructorWelcome.java
@@ -0,0 +1,14 @@
+package me.seg.fitbites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InstructorWelcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_instructor_welcome);
+    }
+}
