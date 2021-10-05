e85b5ac96f4fbbe34f32347971d6ae8b8cfbc777
ritvik130 <rjoha091@uottawa.ca>
2021-10-05 16:03:45 -0400

    Cancel Class Layout

---
+++ project/app/src/main/java/me/seg/fitbites/CancelClass.java
@@ -0,0 +1,14 @@
+package me.seg.fitbites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CancelClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cancel_class);
+    }
+}
