a0c72ce19f5f06cdd9d8fc2b2424d2a48738c04f
ritvik130 <rjoha091@uottawa.ca>
2021-10-06 19:54:08 -0400

    AdminLogin Button functionality

--- project/app/src/main/java/me/seg/fitbites/ManageClassScreen.java
+++ project/app/src/main/java/me/seg/fitbites/ManageClassScreen.java
@@ -3,16 +3,13 @@ package me.seg.fitbites;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
-public class OptionScreen extends AppCompatActivity {
-
+public class ManageClassScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_class_screen);
+
     }
-
-
-
-
 }
