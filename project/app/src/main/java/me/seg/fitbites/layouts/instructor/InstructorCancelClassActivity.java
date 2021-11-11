45bd2a2dda765a9010669e7b6d36b4968f8abc21
AlexandraTulchinsky <alexandratulchinsky@gmail.com>
2021-11-11 13:57:47 -0500

    cancel class

--- project/app/src/main/java/me/seg/fitbites/layouts/instructor/InstructorCancelClassActivity.java
+++ project/app/src/main/java/me/seg/fitbites/layouts/instructor/InstructorCancelClassActivity.java
@@ -2,6 +2,7 @@ package me.seg.fitbites.layouts.instructor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,11 +11,14 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import me.seg.fitbites.MainCreateAccountActivity;
 import me.seg.fitbites.R;
+import me.seg.fitbites.data.Days;
 import me.seg.fitbites.data.FitClass;
 import me.seg.fitbites.data.FitClassType;
 import me.seg.fitbites.firebase.FirestoreDatabase;
 import me.seg.fitbites.firebase.OnTaskComplete;
+import me.seg.fitbites.layouts.admin.AdminClassOptionsActivity;
 import me.seg.fitbites.layouts.admin.AdminEditClassActivity;
 import me.seg.fitbites.layouts.admin.AdminEditClass_SearchActivity;
 
@@ -34,90 +38,102 @@ public class InstructorCancelClassActivity extends AppCompatActivity {
         searchBTN = findViewById(R.id.searchButton);
         backBTN = findViewById(R.id.search_class_back_btn);
 
+        final InstructorCancelClassActivity current=this;
 
-
-    }
         backBTN.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Intent i = new Intent();
-            startActivity(i);
-        }
-    });
-
-
-        //FirestoreDatabase.getInstance().deleteFitClass((FitClass) RemoveClass.getText());
-  searchBTN.setOnClickListener(new View.OnClickListener()
-
-    {
-        @Override
-        public void onClick (View v){
-        FitClass.searchClass(RemoveClass.getText().toString(), new OnTaskComplete<FitClass[]>() {
             @Override
-            public void onComplete(FitClass[] result) {
-
-                //output classes in array into search box
-
-
-                //resultList.toArray(new FitClass[resultList.size()]))
-                placeIntoResults(result);
+            public void onClick(View v) {
+              //  Intent i = new Intent(current,);//TODO ?
+               // startActivity(i);
             }
         });
 
 
+        searchBTN.setOnClickListener(new View.OnClickListener()
+
+        {
+            @Override
+            public void onClick (View v){
+                FitClass.searchClass(RemoveClass.getText().toString(), new OnTaskComplete<FitClass[]>() {
+                    @Override
+                    public void onComplete(FitClass[] result) {
+
+                        placeIntoResults(result);
+                    }
+                });
+
+
+            }
+
+        });
+
     }
 
-    });
 
 
-    public void placeIntoResults(FitClass[] r){
-        LinearLayout layout = (LinearLayout)findViewById(R.id.layout2);
-        LinearLayout.LayoutParams layoutP= new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+    public void placeIntoResults(FitClass[] r) {
+        LinearLayout layout = (LinearLayout) findViewById(R.id.layout2);
+        LinearLayout.LayoutParams layoutP = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
 
         layout.removeAllViews();
         boolean addedAtLeastOne = false;
 
-        for (FitClass c: r){
+        for (FitClass c : r) {// class description
             addedAtLeastOne = true;
-            Button button= new Button(this);
-            button.setText(c.getTime());
-            button.setText(c.getDifficulty());
-            button.setText(c.getDate());
-            button.setText(c.getCapacity());
+            Button button = new Button(this);
+
+            int time = c.getTime();
+            String difficulty = c.getDifficulty().toString();
+            String date = c.getDate().toStringShort();
+            int capacity = c.getCapacity();
+
             FirestoreDatabase.getInstance().getFitClassType(c.getUid(), new OnTaskComplete<FitClassType>() {
+                @SuppressLint("SetTextI18n")
                 @Override
                 public void onComplete(FitClassType result) {
-                    button.setText(result.getClassName());
-                   ;
-                }
-            });
+                    String classname = result.getClassName();
 
+                    button.setText(classname + "/n" + date + "/n" + time + "/n" + difficulty + "/n" + capacity);
 
-            //
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-
-                    //click remove
-                    //delete class from database
-
-
-
-
-
-
-                    Intent intent = new Intent(AdminEditClass_SearchActivity.this, AdminEditClassActivity.class);
-                    intent.putExtra("uid",c.getUid());
-                    startActivity(intent);
                 }
             });
             layout.addView(button, layoutP);
+
+
+            button.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View v) { //won't do anything until remove is clicked
+
+
+
+                    removeBTN.setOnClickListener(new View.OnClickListener() {//delete FitClass
+                        @Override
+                        public void onClick(View v) {
+
+                            //remove from db
+
+                            FirestoreDatabase.getInstance().deleteFitClass(c);
+
+                        }
+                    });
+
+
+                }
+            });
+
+
         }
 
 
 
 
+
     }
+
+
+
 
 }
 
