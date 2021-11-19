b88fe46a3fd317cf47b1fd6f075717118205b4cc
Xingjian <jasonsu93272@gmail.com>
2021-11-19 17:57:20 -0500

    search layout front and back

--- project/app/src/main/java/me/seg/fitbites/data/Difficulty.java
+++ project/app/src/main/java/me/seg/fitbites/data/Difficulty.java
@@ -2,20 +2,7 @@ package me.seg.fitbites.data;
 
 public enum Difficulty {
 
-    BEGINNER("BEGINNER");
-    INTERMEDIATE(level:"INTERMEDIATE");
-    EXPERIENCED(level: "EXPERIENCED");
-
-
-    private String level;
-
-    private Difficulty(String level) {
-        this.level = level;
-    }
-
-    public String toString() {
-        return level;
-    }
+    BEGINNER, INTERMEDIATE, EXPERIENCED;
 
 
 }
---
+++ project/app/src/main/java/me/seg/fitbites/firebase/OnObjectFilled.java
@@ -0,0 +1,9 @@
+package me.seg.fitbites.firebase;
+
+import java.util.ArrayList;
+
+import me.seg.fitbites.data.ClassInfoDisplay;
+
+public interface OnObjectFilled {
+    public void onObjectReady();
+}
