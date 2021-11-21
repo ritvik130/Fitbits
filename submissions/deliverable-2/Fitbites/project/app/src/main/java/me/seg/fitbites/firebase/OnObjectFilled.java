7404e7e9c8b450ea4e367e0138c43efc05659199
KyleDulce <36104936+KyleDulce@users.noreply.github.com>
2021-11-21 15:57:53 -0500

    Removed non-needed folder

--- submissions/deliverable-2/Fitbites/project/app/src/main/java/me/seg/fitbites/firebase/OnObjectFilled.java
+++
@@ -1,9 +0,0 @@
-package me.seg.fitbites.firebase;
-
-import java.util.ArrayList;
-
-import me.seg.fitbites.data.ClassInfoDisplay;
-
-public interface OnObjectFilled {
-    public void onObjectReady();
-}
--- submissions/deliverable-2/Fitbites/project/app/src/main/java/me/seg/fitbites/firebase/OnTaskComplete.java
+++
@@ -1,5 +0,0 @@
-package me.seg.fitbites.firebase;
-
-public interface OnTaskComplete<T> {
-    public void onComplete(T result);
-}
